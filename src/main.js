6a34f7faf7fdce21a0b117f4dcee4ed70a6f059a
Cancerleo <sandra.iriandini@gmail.com>
2020-06-21 22:21:36 +0200

    Import Forecast and Gradient components

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Gradient from "./components/Gradient";
+import Forecast from "./components/Forecast";
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
 
-Vue.config.productionTip = false
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
+Vue.config.productionTip = false;
+Vue.component("Gradient", Gradient);
+Vue.component("Forecast", Forecast);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
