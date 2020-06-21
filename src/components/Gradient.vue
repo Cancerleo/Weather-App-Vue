6c94063596e9bb6ee8443f8ca0aaf269cfca13df
Cancerleo <sandra.iriandini@gmail.com>
2020-06-21 22:21:14 +0200

    create Gradient component

---
+++ src/components/Gradient.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="gradient"></div>
+</template>
+<script>
+export default {
+  methods: {
+    update(temp) {
+      let maxRange = -400;
+
+      let coefficient = (temp + 40) / 80;
+      coefficient = Math.max(0, Math.min(1, coefficient));
+      console.log(
+        `We should offset the gradient by ${maxRange * (1 - coefficient)}`
+      );
+      var gradient = document.getElementById("gradient");
+      gradient.style.top = `${maxRange * (1 - coefficient)}%`;
+      console.log(gradient.style.top);
+    },
+  },
+};
+</script>
