6dfc9018f418b484583483620a96bb4f48d65fb5
Cancerleo <sandra.iriandini@gmail.com>
2020-06-21 22:21:01 +0200

    Create forecast component

---
+++ src/components/Forecast.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>{{ dateBuilder(date) }}</h1>
+    <div>{{ temp }} ℃</div>
+  </div>
+</template>
+<script>
+export default {
+  name: "forecast",
+  props: {
+    date: Date,
+    temp: Number,
+  },
+  methods: {
+    dateBuilder: function(d) {
+      // let months = [
+      //   "January",
+      //   "February",
+      //   "March",
+      //   "April",
+      //   "Mei",
+      //   "June",
+      //   "July",
+      //   "August",
+      //   "September",
+      //   "October",
+      //   "November",
+      //   "December",
+      // ];
+      let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+
+      let day = days[d.getDay()];
+
+      return `${day}`;
+    },
+  },
+};
+</script>
